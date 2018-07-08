359fee656cb46c3b8f8b9f4ea79d4964f0d6d0b8
Hilvcha <micah_wang@163.com>
2018-07-09 02:47:11 +0800

    clean

--- Private_Doctor/private_doctor/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from private_doctor import models
-admin.site.register(models.Doctor)
-admin.site.register(models.Family)
-admin.site.register(models.Family_Doctor)
-admin.site.register(models.Appointment)
--- private_doctor/private_doctor/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class PrivateDoctorConfig(AppConfig):
-    name = 'private_doctor'
